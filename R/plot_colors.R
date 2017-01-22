aca0a2f35b234aeee0f3a31cc8fc829f2f39317e
RebeccaStubbs <stubbsrw@gmail.com>
2017-01-21 20:42:08 -0800

    update documentation, rename package

--- R/plot_colors.R
+++ R/plot_colors.R
@@ -9,7 +9,7 @@
 #' @return ggplot of color swatches and optional name of color pallette
 #' @export
 #'
-#' @examples plot_colors(c("red,"yellow","blue"),"primary colors")
+#' @examples plot_colors(c("red","yellow","blue"),"primary colors")
 
 plot_colors<-function(color_list,color_list_name=NULL){
   # color_list: list of colors
@@ -19,7 +19,7 @@ plot_colors<-function(color_list,color_list_name=NULL){
   data_table<-data.table(data.frame(x=letters[1:length(unique(color_list))]))
   data_table[, colors:=unique(color_list)]
   data_table[, bar_height:=1]
-  p<-ggplot(data_table,aes(x=colors))+geom_bar(aes(fill=colors, stat="bin"))+theme_tufte()+labs(x=paste(unique(color_list), collapse=', '))+
+  p<-ggplot(data_table,aes(x=colors))+geom_bar(aes(fill=colors))+theme_tufte()+labs(x=paste(unique(color_list), collapse=', '))+
     theme(
       legend.position = "none",
       axis.title.y = element_blank(),
